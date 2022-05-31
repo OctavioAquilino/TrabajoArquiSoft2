a358ea2b1b2425adb4c5ad995c1b5600308d0dfc
Santiago Riveros Salomón <Santiagosalomon2001@gmail.com>
2022-05-31 15:56:44 -0300

    cambios

--- mvc-go/dto/dto_product.go
+++ mvc-go/dto/dto_product.go
@@ -3,10 +3,11 @@ package dto
 type ProductDto struct {
 	Name string `json:"name"`
 	//UniversalCode string      `json:"universal_code"`
-	Picture    string  `json:"picture_url"`
-	Price      float32 `json:"base_price"`
-	Id         int     `json:"id"`
-	IdCategory int     `json:"id_category"`
+	Picture     string  `json:"picture_url"`
+	Price       float32 `json:"base_price"`
+	Id          int     `json:"id"`
+	IdCategory  int     `json:"id_category"`
+	Description string  `json:"description"`
 }
 
 type ProductsDto []ProductDto
--- mvc-go/model/product.go
+++ mvc-go/model/product.go
@@ -5,8 +5,9 @@ type Product struct {
 	Name  string  `gorm:"type:varchar(350);not null"`
 	Price float32 `gorm:"type:float(150);not null"`
 	//UniversalCode string  `gorm:"type:varchar(350);not null"`
-	Picture    string `gorm:"type:varchar(350);not null"`
-	IdCategory int    `gorm:"type:integer;not null"` //chequear lo de integer
+	Picture     string `gorm:"type:varchar(350);not null"`
+	IdCategory  int    `gorm:"type:integer;not null"` //chequear lo de integer
+	Description string `gorm:"type:varchar(350);not null"`
 }
 
 type Products []Product
