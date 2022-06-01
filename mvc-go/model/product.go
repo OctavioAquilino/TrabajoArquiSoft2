cf09c79f77f621e7c3a82f1c9b72255882c8e1f6
OctavioAquilino <tayiaquilino12345@gmail.com>
2022-06-01 09:50:40 -0300

    .

--- mvc-go/model/product.go
+++ mvc-go/model/product.go
@@ -3,7 +3,7 @@ package model
 type Product struct {
 	Id    int     `gorm:"primaryKey"`
 	Name  string  `gorm:"type:varchar(350);not null"`
-	Price float32 `gorm:"type:float(150);not null"`
+	Price float32 `gorm:"type:decimal(60,4);not null"`
 	//UniversalCode string  `gorm:"type:varchar(350);not null"`
 	Picture     string `gorm:"type:varchar(350);not null"`
 	IdCategory  int    `gorm:"type:integer;not null"` //chequear lo de integer
