bf35f0a6aa3f290b6b5d3b3ca74eb4eda458d3f3
OctavioAquilino <tayiaquilino12345@gmail.com>
2022-06-04 21:11:01 -0300

    Model de product arreglado

--- mvc-go/model/product.go
+++ mvc-go/model/product.go
@@ -1,14 +1,13 @@
 package model
 
 type Product struct {
-	Id    int     `gorm:"primaryKey"`
-	Name  string  `gorm:"type:varchar(350);not null"`
-	Price float32 `gorm:"type:decimal(60,4);not null"`
-	//UniversalCode string  `gorm:"type:varchar(350);not null"`
-	Picture     string `gorm:"type:varchar(350);not null"`
-	Stock       int    `gorm:"type:integer;not null"`
-	IdCategory  int    `gorm:"type:integer;not null"` //chequear lo de integer
-	Description string `gorm:"type:varchar(350);not null"`
+	Id          int     `gorm:"primaryKey"`
+	Name        string  `gorm:"type:varchar(350);not null"`
+	Price       float32 `gorm:"type:decimal(60,4);not null"`
+	Picture     string  `gorm:"type:varchar(350);not null"`
+	Stock       int     `gorm:"type:int(150);not null"`
+	IdCategory  int     `gorm:"type:int(150);not null"`
+	Description string  `gorm:"type:varchar(350);not null"`
 }
 
 type Products []Product
