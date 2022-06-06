7accf8eafd997c8736979c4b696ad3d98ed3176a
Santiago Riveros Salomón <Santiagosalomon2001@gmail.com>
2022-06-05 21:29:46 -0300

    Se realizo el buscador

--- Pagina/src/components/Productos/index.js
+++ Pagina/src/components/Productos/index.js
@@ -5,19 +5,43 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export const ProductosLista = ()=>{
 
     const [productos,setProductos] = useState([]);
+    const [prodseach,setProdseach] = useState([]);
+    const [busqueda, setBusqueda]= useState("");
     const fetchApi = async()=>{
     const response = await fetch('http://localhost:8090/product')
     .then((response) => response.json());
     setProductos(response);
+    setProdseach(response);
     };
     useEffect(()=>{
     fetchApi();
     },[])
+    const handleChange=e=>{
+        setBusqueda(e.target.value);
+        filtrar(e.target.value);
+      }
+      const filtrar=(terminoBusqueda)=>{
+        var resultadosBusqueda=prodseach.filter((elemento)=>{
+          if(elemento.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase()))
+          {
+            return elemento;
+          }
+        });
+        setProductos(resultadosBusqueda);
+      }
 
     return(
         <>
         <h1 className="title"> PRODUCTOS</h1>
-        
+        <div className="containerInput">
+        <input
+          className="form-control inputBuscar"
+          value={busqueda}
+          placeholder="Buscador de Productos"
+          onChange={handleChange}
+        />
+      </div>
+
         <div className="productos">
             {
                 productos.map(producto =>(
