bd91738ca48a36b6fe44cb2cc80b8cec4a728fb3
Santiago Riveros Salomón <Santiagosalomon2001@gmail.com>
2022-06-08 20:45:08 -0300

    se arreglo los botones de las categorias

--- Pagina/src/components/Inicio/index.js
+++ Pagina/src/components/Inicio/index.js
@@ -2,53 +2,18 @@ import React, {useEffect, useState } from "react";
 import { CategoryItem } from "./CategoryItem";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ProductoItem } from "../Productos/ProductoItem";
-import { Cookies } from "react-cookie";
 
 async function GetProductByIdCategory(id) {
-    return fetch('http://localhost:8090/productCategory/'+id, {
+    return fetch('http://localhost:8090/productCategory/' +id, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
       }
     })
       .then(data => data.json())
-      /*.then(data => {
-          console.log(data)
-      })*/
    }
 
-
-function RenderStart(){
-    "No hay Productos en la Categoria"    
-}
-/*function RenderProducts(){
-
-    <div className="productos">
-    {
-            productos.map(producto =>(
-              <ProductoItem key={producto.id}
-              id={producto.id}
-              name={producto.name}
-              base_price={producto.base_price}
-              id_category={producto.id_category}
-              stock={producto.stock}
-              picture_url={producto.picture_url}
-              description={producto.description}
-              />  
-            ))
-    }
-    </div>
-    alert("holaaaa")
-}*/
-/* return if(productos){
-    RenderProducts
-
-} else{
-    RenderStart
-}
-*/
-export const CategoryLista =()=>{
-    //const Cookie = new Cookies();    
+export const CategoryLista =()=>{   
     
     const [categorias,setCategorias] = useState([]);
     const fetchApi = async()=>{
@@ -65,13 +30,7 @@ export const CategoryLista =()=>{
     const response = await GetProductByIdCategory(id)
     setProductos(response)
     console.log(response);
-
-    
     };
-    /*useEffect(() => {
-        Handle();
-    }, [])*/
-
 const Render =(
     <div className="productos">
     {
@@ -89,7 +48,6 @@ const Render =(
     }
     </div> 
 )
-
     return(
         <>
         <h1 className="title"> CATEGORIAS</h1>
@@ -99,15 +57,15 @@ const Render =(
                   <button className="category"onClick={()=>Handle(categoria.id_category)}>
                       <CategoryItem key={categoria.id_category}
                   name={categoria.nombre}
-                  description={categoria.descripcion}
                   />
                   </button>
                 ))
+                
             }
         </div>
-        {productos? RenderStart():Render}  
+          <div>
+              {productos>0? "No hay Productos en esta categoria": Render}
+          </div>
         </>
     );
 }
-
-
