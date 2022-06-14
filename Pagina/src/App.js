0c042940e498b80659cee1da7ec6c9108fb383fc
NicoRojo562 <2000262@ucc.edu.ar>
2022-06-14 16:07:03 -0300

    app.js

--- Pagina/src/App.js
+++ Pagina/src/App.js
@@ -3,19 +3,18 @@ import {Header} from './components/Header';
 import 'boxicons';
 import {BrowserRouter as Router} from "react-router-dom";
 import {Paginas} from './components/Paginas';
-import {Carrito} from './components/Carrito/index';
-import {Dataprovider} from './components/Carrito/Provider';
+//import {Carrito} from './components/Carrito/index';
+//import {Dataprovider} from './components/Carrito/Provider';
 function App() {
   return (
-    <Dataprovider>
+    
     <div className="App">
       <Router>
         <Header/>
-        <Carrito/>
         <Paginas/>
       </Router>
     </div>
-  </Dataprovider>
+ 
   );
 }
 
